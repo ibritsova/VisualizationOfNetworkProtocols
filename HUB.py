a45d5b9388d51eab61f7aa6b3946036f0843b062
unknown <ira.britsova18112004@gmail.com>
2024-01-20 21:23:37 +0100

    ImprovedClasses

--- HUB.py
+++ HUB.py
@@ -1,29 +1,62 @@
 import pygame
 from visualization.Node import Node
+from visualization.EndPoint import EndPoint
 
 
 class HUB(Node):
+    endPoints = []
+    subHubs = []
+    packetsOnTheWay = []
+    recievedPackets = []
 
-
-    def __init__(self, x, y, name, info = "", previousNode = None):
+    def __init__(self, x, y, name, info = ""):
         super().__init__(x, y, name, 40, (0, 255, 0), info)
         self.text_bg_color = (200, 200, 200)
         self.img = pygame.image.load("hub.png")
         new_size = (50, 50)
+        self.endPoints = []
+        self.subHubs = []
         self.img = pygame.transform.scale(self.img, new_size)
-        self.previousNode = previousNode
 
+    def addEndPoint(self, node):
+        self.endPoints.append(node)
+
+
+    def addSubHub(self, hub):
+        self.subHubs.append(hub)
 
     def receive(self, packet):
+        self.recievedPackets.append(packet)
+        self.send(packet)
         print(f"{self.name} received a packet")
 
     def send(self, packet):
+        for endPoint in self.endPoints:
+            self.packetsOnTheWay.append((packet, endPoint))
+        for subHub in self.subHubs:
+            self.packetsOnTheWay.append((packet, subHub))
         print(f"Sending packet from {self.name}")
 
-    def connect(self, screen):
+    def updateSending(self):
+        for tuple in self.packetsOnTheWay:
+            if tuple[0].x == tuple[1].x and tuple[0].y == tuple[1].y:
+                if type(tuple[1]) == HUB:
+                    tuple[1].recieve(tuple[0])
+            else:
+                self.move(tuple[0], tuple[1])
+
+    def move(self, packet, node):
+         # TODO: implements
+
+    def connect(self, screen, nodeToConnect):
         line_color = (255, 0, 0)
-        pygame.draw.line(screen, line_color, (self.x, self.y), (self.previousNode.x, self.previousNode.y), 2)
-        # print(f"Coordinates: {(self.x, self.y)} -> {(self.previousNode.x, self.previousNode.y)}")
+        pygame.draw.line(screen, line_color, (self.x, self.y), (nodeToConnect.x, nodeToConnect.y), 2)
+
+    def connectToEndPoints(self, screen):
+        line_color = (255, 0, 0)
+        for endPoint in self.endPoints:
+            endPoint.draw(screen)
+            pygame.draw.line(screen, line_color, (self.x, self.y), (endPoint.x, endPoint.y), 2)
 
     def onMouseOver(self, mouse_pos, screen):
 
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import pygame
 import sys
 
@@ -6,6 +5,7 @@ from visualization.EndPoint import EndPoint
 from visualization.HUB import HUB
 from visualization.Packet import Packet
 from visualization.InputBox import InputBox
+
 
 class Button:
     def __init__(self, x, y, width, height, color, text, node_type):
@@ -23,7 +23,8 @@ class Button:
     def isMouseOver(self):
         return self.rect.collidepoint(pygame.mouse.get_pos())
 
-def draw_nodes(nodes, buttons):
+
+def draw_nodes(hubs, buttons):
     pygame.init()
 
     screen = pygame.display.set_mode((1000, 900))
@@ -33,20 +34,19 @@ def draw_nodes(nodes, buttons):
     inputBox1 = InputBox(0, 100, 100, 100)
     inputBox1.draw(screen)
 
-
-
     x = 80
     y = 150
-    previous = None
+
     first = True
     inputBox = None
-    # input_box2 = InputBox(100, 300, 140, 32)
 
     while True:
         for event in pygame.event.get():
-            for node in nodes:
-                node.update_position(event)
-
+            # responsible for dragging elements
+            for hub in hubs:
+                hub.update_position(event)
+                for endPoint in hub.endPoints:
+                    endPoint.update_position(event)
 
             if inputBox is not None:
                 inputBox.handle_event(event)
@@ -59,116 +59,72 @@ def draw_nodes(nodes, buttons):
                 for button in buttons:
                     if button.rect.collidepoint(event.pos):
                         if button.node_type == "Delete":
-                            nodes = []
-                            previous = None
-                            current = None
+                            hubs = []
                             x = 80
                             y = 150
                     if button.isMouseOver():
                         if button.node_type == "HUB":
-                           x += 100
-                           if first:
-                               first = False
-                               previous = HUB(x, y, "HUB", "Pridame neskor")
-                               nodes.append(previous)
-                           else:
-                               current = HUB(x, y, "HUB", "Pridame neskor", previous)
-                               nodes.append(current)
-                               previous = current
+                            x += 100
+                            newHub = HUB(x, y, "HUB", "Pridame neskor")
+                            hubs.append(newHub)
+                            if(len(hubs) > 0):
+                                hubs[-1].addSubHub(newHub)
+
+
 
 
 
                         elif button.node_type == "Packet":
                             x += 100
-                            # pygame.draw.rect(screen, "black",
-                            #                  (100, 100, 60, 100))
-                            if inputBox is None:
-                                inputBox = InputBox(100, 700, 200, 30)
-                                # print("Case1")
-                            inputBox.active = True
-                            if(inputBox == None):
-                                inputBox = InputBox(event.pos[0], event.pos[1], 200, 30)
-                                # print("Case2")
-                            #inputBox.draw(screen)
-                            # rect = pygame.Rect(x, y, 50, 100)
-                            # color = pygame.Color('black')
-                            # pygame.draw.rect(screen, color, rect, 20)
-                            # print("Kreslim inputbox")
-                            nodes.append(Packet(x, y, "Packet", "Pridame neskor"))
+                            # TODO: fix inputBox
+                            # if inputBox is None:
+                            #     inputBox = InputBox(100, 700, 200, 30)
+                            #
+                            # inputBox.active = True
+                            # if (inputBox == None):
+                            #     inputBox = InputBox(event.pos[0], event.pos[1], 200, 30)
+
+                            packet = Packet(x, y, "Packet", "Pridame neskor")
+                            packet.draw(screen)
 
 
                         elif button.node_type == "EndPoint":
                             y += 100
+                            hub = hubs[-1]
+                            hub.addEndPoint(EndPoint(x,y, "EndPoint", "Pridame neskor"))
 
-                            if first:
-                                first = False
-                                previous = EndPoint(x, y, "EndPoint", "Pridame neskor")
-                                nodes.append(previous)
-                            else:
-                                current = EndPoint(x, y, "EndPoint", "Pridame neskor", previous)
-                                nodes.append(current)
-                                # previous = current
+                        elif button.node_type == "Quit":
+                            pygame.quit()
+                            sys.exit()
 
-        # input_box2.update()
-        # input_box2.draw(screen)
         screen.fill((255, 255, 255))
 
-        for node in nodes:
-            if node.isMouseOver():
+        for hub in hubs:
+            if hub.isMouseOver():
                 font = pygame.font.Font(None, 24)
-                text = font.render(node.get_info(), True, (0, 0, 0), node.text_bg_color)
-                screen.blit(text, (node.x - node.size // 2, node.y - node.size // 2 - 20))
+                text = font.render(hub.get_info(), True, (0, 0, 0), hub.text_bg_color)
+                screen.blit(text, (hub.x - hub.size // 2, hub.y - hub.size // 2 - 20))
 
-        for node in nodes:
-            if isinstance(node, HUB) or isinstance(node, EndPoint):
-                node.draw(screen)
-                if node.previousNode is not None:
-                    node.connect(screen)
-            else:
-                node.draw(screen)
-
-
+        for i in range(len(hubs)):
+            hubs[i].draw(screen)
+            hubs[i].connectToEndPoints(screen)
+            if(i > 0):
+                hubs[i].connect(screen, hubs[i - 1])
 
 
         for button in buttons:
             button.draw(screen)
 
         pygame.display.flip()
-        clock.tick(60)
-# def main():
-#     clock = pygame.time.Clock()
-#     pygame.init()
-#
-#     screen = pygame.display.set_mode((1000, 900))
-#     input_box1 = InputBox(100, 100, 140, 32)
-#     input_box2 = InputBox(100, 300, 140, 32)
-#     input_boxes = [input_box1, input_box2]
-#     done = False
-#
-#     while not done:
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 done = True
-#             for box in input_boxes:
-#                 box.handle_event(event)
-#
-#         for box in input_boxes:
-#             box.update()
 
-#
-#         screen.fill((30, 30, 30))
-#         for box in input_boxes:
-#             box.draw(screen)
-#
-#         pygame.display.flip()
-#         clock.tick(30)
+
 if __name__ == "__main__":
-    nodes = []
+    hubs = []
     buttons = [
         Button(200, 50, 100, 50, (0, 0, 0), "HUB", "HUB"),
         Button(350, 50, 100, 50, (0, 0, 0), "Packet", "Packet"),
         Button(500, 50, 150, 50, (0, 0, 0), "EndPoint", "EndPoint"),
-        Button(700, 50, 200, 50, (0, 0, 0), "Delete all", "Delete")
+        Button(700, 50, 200, 50, (0, 0, 0), "Delete all", "Delete"),
+        Button(900, 300, 100, 50, (0, 0, 0), "Quit", "Quit")
     ]
-    draw_nodes(nodes, buttons)
-    #main()
+    draw_nodes(hubs, buttons)
